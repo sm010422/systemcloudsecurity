dd9341ffd0e46f974589bd0901c89f641bd0c6bb
Park Sang Min <sm0104224@gmail.com>
2026-04-02 14:17:17 +0900

    new version -> using execve

---
+++ catall_execve.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char *v[3];
+
+    if(argc < 2) {
+        printf("Please type a file name.\n");
+        return 1;
+    }
+
+    v[0] = "/bin/cat";
+    v[1] = argv[1]; 
+    v[2] = 0;
+    execve(v[0], v, 0);
+
+    return 0;
+}
